74f7d51148956052c82c8730a8a27365a3879643
Alejandro CH <alejancruzhdez@gmail.com>
2023-05-16 00:00:47 -0600

    q

--- App.py
+++ App.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, make_response
-from reportlab.pdfgen import canvas
-from io import BytesIO
+from fpdf import FPDF
 from Supa import datos
 import os
 import dotenv
@@ -24,7 +23,7 @@ def home():
 @app.route('/contacto', methods=['GET', 'POST'])
 def contacto():
     if request.method == 'POST':
-        # Obtener los datos del formulario
+                # Obtener los datos del formulario
         nombre = request.form.get('nombre')
         email = request.form.get('email')
         mensaje = request.form.get('mensaje')
@@ -34,31 +33,29 @@ def contacto():
         response.headers['Content-Disposition'] = 'attachment; filename=contacto.pdf'
         return response
 
+
     return render_template('contacto.html')
 
 @app.route('/catalogo')
 def catalog():
     return render_template('catalogo.html', items=items)
 
+
 def generate_pdf(nombre, email, mensaje):
-    # Crear un objeto PDFCanvas
-    buffer = BytesIO()
-    c = canvas.Canvas(buffer)
+    # Crear un objeto PDF
+    pdf = FPDF()
+    pdf.add_page()
 
     # Agregar contenido al PDF
-    c.setFont("Helvetica", 12)
-    c.drawString(100, 700, "Nombre: {}".format(nombre))
-    c.drawString(100, 680, "Email: {}".format(email))
-    c.drawString(100, 660, "Mensaje: {}".format(mensaje))
-
-    # Finalizar y guardar el PDF
-    c.showPage()
-    c.save()
+    pdf.set_font("Helvetica", size=12)
+    pdf.cell(0, 10, txt="Nombre: {}".format(nombre), ln=1)
+    pdf.cell(0, 10, txt="Email: {}".format(email), ln=1)
+    pdf.cell(0, 10, txt="Mensaje: {}".format(mensaje), ln=1)
 
     # Obtener los datos del buffer y crear una respuesta Flask
-    buffer.seek(0)
-    response = make_response(buffer.getvalue())
-    response.mimetype = 'application/pdf'
+    response = make_response(pdf.output(dest='S').encode('latin1'))
+    response.headers.set('Content-Disposition', 'attachment', filename='contacto.pdf')
+    response.headers.set('Content-Type', 'application/pdf')
     return response
 
 if __name__ == '__main__':
