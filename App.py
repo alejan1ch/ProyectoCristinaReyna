044fe6f9004793cb926ea3fb6c1e30afaf7533de
Alejandro CH <alejancruzhdez@gmail.com>
2023-05-15 22:59:43 -0600

    new

--- App.py
+++ App.py
@@ -4,62 +4,62 @@ from io import BytesIO
 from Supa import datos
 import os
 import dotenv
-
-def create_app():
-    app = Flask(__name__)
-    dotenv.load_dotenv()
+app = Flask(__name__)
+dotenv.load_dotenv()
 
 
-    # Variable global para almacenar los datos
-    items = []
+# Variable global para almacenar los datos
+items = []
 
-    # Función para cargar los datos antes de cada solicitud
-    @app.before_request
-    def cargar_datos():
-        global items
-        items = datos(os.environ.get('URL'),os.environ.get('KEY'))
+# Función para cargar los datos antes de cada solicitud
+@app.before_request
+def cargar_datos():
+    global items
+    items = datos(os.environ.get('URL'),os.environ.get('KEY'))
 
-    @app.route('/')
-    def home():
-        return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    @app.route('/contacto', methods=['GET', 'POST'])
-    def contacto():
-        if request.method == 'POST':
-            # Obtener los datos del formulario
-            nombre = request.form.get('nombre')
-            email = request.form.get('email')
-            mensaje = request.form.get('mensaje')
+@app.route('/contacto', methods=['GET', 'POST'])
+def contacto():
+    if request.method == 'POST':
+        # Obtener los datos del formulario
+        nombre = request.form.get('nombre')
+        email = request.form.get('email')
+        mensaje = request.form.get('mensaje')
 
-            # Generar el archivo PDF
-            response = generate_pdf(nombre, email, mensaje)
-            response.headers['Content-Disposition'] = 'attachment; filename=contacto.pdf'
-            return response
-
-        return render_template('contacto.html')
-
-    @app.route('/catalogo')
-    def catalog():
-        return render_template('catalogo.html', items=items)
-
-    def generate_pdf(nombre, email, mensaje):
-        # Crear un objeto PDFCanvas
-        buffer = BytesIO()
-        c = canvas.Canvas(buffer)
-
-        # Agregar contenido al PDF
-        c.setFont("Helvetica", 12)
-        c.drawString(100, 700, "Nombre: {}".format(nombre))
-        c.drawString(100, 680, "Email: {}".format(email))
-        c.drawString(100, 660, "Mensaje: {}".format(mensaje))
-
-        # Finalizar y guardar el PDF
-        c.showPage()
-        c.save()
-
-        # Obtener los datos del buffer y crear una respuesta Flask
-        buffer.seek(0)
-        response = make_response(buffer.getvalue())
-        response.mimetype = 'application/pdf'
+        # Generar el archivo PDF
+        response = generate_pdf(nombre, email, mensaje)
+        response.headers['Content-Disposition'] = 'attachment; filename=contacto.pdf'
         return response
-    return app
+
+    return render_template('contacto.html')
+
+@app.route('/catalogo')
+def catalog():
+    return render_template('catalogo.html', items=items)
+
+def generate_pdf(nombre, email, mensaje):
+    # Crear un objeto PDFCanvas
+    buffer = BytesIO()
+    c = canvas.Canvas(buffer)
+
+    # Agregar contenido al PDF
+    c.setFont("Helvetica", 12)
+    c.drawString(100, 700, "Nombre: {}".format(nombre))
+    c.drawString(100, 680, "Email: {}".format(email))
+    c.drawString(100, 660, "Mensaje: {}".format(mensaje))
+
+    # Finalizar y guardar el PDF
+    c.showPage()
+    c.save()
+
+    # Obtener los datos del buffer y crear una respuesta Flask
+    buffer.seek(0)
+    response = make_response(buffer.getvalue())
+    response.mimetype = 'application/pdf'
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Supa.py
+++ Supa.py
@@ -1,7 +1,29 @@
+import os
+from dotenv import load_dotenv
 import supabase
 
+# carga las variables de entorno desde el archivo .env
+load_dotenv()
+
+# obtiene las variables de entorno
+SUPABASE_URL = "https://ymhtktsffxmaajexzney.supabase.co/rest/v1/Productos?select=*"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltaHRrdHNmZnhtYWFqZXh6bmV5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4NDA0NDcyNCwiZXhwIjoxOTk5NjIwNzI0fQ.aw9AKo_rs39WTNGjorSexD79k-Izt5cmUdDCnBcVXfM"
+
+# crea una instancia de cliente Supabase
+client = supabase.create_client(SUPABASE_URL, SUPABASE_KEY)
+
+# consulta los productos de la tabla Productos
+response = client.from_('Productos').select('*').execute()
+
+# imprime los datos obtenidos
+
+
+
+
 def datos(url, clave):
-    client = supabase.create_client(url, clave)
+    SUPABASE_URL = url
+    SUPABASE_KEY = clave
+    client = supabase.create_client(SUPABASE_URL, SUPABASE_KEY)
     response = client.from_('Productos').select('*').execute()
     lista= str(response)
     texto_formateado= lista
